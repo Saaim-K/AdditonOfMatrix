f6ae4ba7db6fc64b21f1d119c1ff47da68ac038f
Saaim Khan <89778620+Saaim-K@users.noreply.github.com>
2022-06-13 22:15:12 +0500

    Matrix Addition App

---
+++ app.js
@@ -0,0 +1,39 @@
+let input1 = ''
+let input2 = ''
+
+function start() {
+    let matrix1 = document.querySelector('#matrix1')
+    let matrix2 = document.querySelector('#matrix2')
+    let resultMatrix = document.querySelector('#resultMatrix')
+
+    matrix1.innerHTML = "";
+    matrix2.innerHTML = "";
+    resultMatrix.innerHTML = "";
+
+    input1 = +document.querySelector('#input1').value
+    input2 = +document.querySelector('#input2').value
+    for (let i = 0; i < input1; i++) {
+        for (let j = 0; j < input2; j++) {
+            matrix1.innerHTML +=
+                `<input type="number" required min="0" max="100" id="m1-${i}-${j}" value='0'>`;
+            matrix2.innerHTML +=
+                `<input type="number" required min="0" max="100" id="m2-${i}-${j}" value='0'>`;
+            resultMatrix.innerHTML +=
+                `<input class='black' type="number" disabled required min="0" max="100" id="resultMatrix-${i}-${j}" value='0'>`
+        }
+        matrix1.innerHTML += `<br>`
+        matrix2.innerHTML += `<br>`
+        resultMatrix.innerHTML += `<br>`
+    }
+    document.querySelector('#bnm').classList.remove('klp')
+}
+function sum() {
+    for (let i = 0; i < input1; i++) {
+        for (let j = 0; j < input2; j++) {
+            document.querySelector(`#resultMatrix-${i}-${j}`).value =
+                +document.querySelector(`#m1-${i}-${j}`).value
+                +
+                +document.querySelector(`#m2-${i}-${j}`).value;
+        }
+    }
+}
